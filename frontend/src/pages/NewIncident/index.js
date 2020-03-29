fa24b8f943f2b358f375af16788bd09b1141eb27
ronaldo.caetano <ronaldo.caetano@acct.global>
2020-03-29 10:59:31 -0300

    Css for profile and incident pages

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+import './styles.css'
+import logoImage from './../../assets/logo.svg'
+
+export default function Profile() {
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImage} alt="Be The Hero" />
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#e02041"/> 
+                        Voltar para Home
+                    </Link>
+                </section>
+                <form action="">
+                    <input placeholder="Título do caso" />
+                    <textarea placeholder="Descrição" />
+                    <input placeholder="Valor em R$" />
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
